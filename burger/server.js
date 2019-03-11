b7036cad7a9a0b13e927710c817d125f9b2d6ae4
Robert O <romohund@gmail.com>
2019-03-11 14:51:35 -0500

    Working product

--- burger/server.js
+++ burger/server.js
@@ -3,15 +3,12 @@ var bodyParser = require("body-parser");
 
 var app = express();
 
-// Set the port of our application
-// process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 8080;
 
-// lets client side files use relative file paths
-// specifically the public folder
+
 app.use(express.static("public"));
 
-// Parse application/x-www-form-urlencoded
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -20,15 +17,13 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// connects api routes to the server
+
 var routes = require("./controllers/burgers_controller");
 
-// allows the app (express) to use the var routes
+
 app.use(routes);
 
-// Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
-    // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
   });
   
